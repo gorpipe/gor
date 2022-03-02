720b22e77a8213f703a182cc429f8d0e785d77d0
Gísli Magnússon <gisli@wuxinextcode.com>
2022-03-02 10:28:03 +0000

    Resolve "Update libs and fix security vulnerabilities"
    (cherry picked from commit 5b16328ebd026a105e7ffabebcd58102cc94bc10)

--- drivers/src/main/java/org/gorpipe/s3/driver/S3ClientFileSystemProvider.java
+++ drivers/src/main/java/org/gorpipe/s3/driver/S3ClientFileSystemProvider.java
@@ -1,11 +1,13 @@
 package org.gorpipe.s3.driver;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.upplication.s3fs.AmazonS3Client;
 import com.upplication.s3fs.S3FileSystemProvider;
 import com.upplication.s3fs.S3FileSystem;
 
 import java.net.URI;
-import java.util.Properties;
+import java.nio.file.FileSystem;
+import java.util.Map;
 
 public class S3ClientFileSystemProvider extends S3FileSystemProvider {
 
@@ -17,7 +19,19 @@ public class S3ClientFileSystemProvider extends S3FileSystemProvider {
      * @param client AmazonS3
      * @return S3FileSystem never null
      */
-    public S3FileSystem createFileSystem(URI uri, Properties props, AmazonS3 client) {
-        return new S3FileSystem(this, getFileSystemKey(uri, props), client, uri.getHost());
+    public FileSystem createFileSystem(URI uri, Map<String, ?> props, AmazonS3 client) {
+        return new S3FileSystem(this, new AmazonS3Client(client), uri.getHost());
+    }
+
+    // We we are not constructing the FileSystem as expected (using the newFileSystem method) as we
+    // want to reuse the client (could change that and stop reusing the client and pass in creds in the props
+    // var)
+    protected boolean isAES256Enabled() {
+        try {
+            return super.isAES256Enabled();
+        } catch (NullPointerException npe) {
+            // ignore
+        }
+        return false;
     }
 }
