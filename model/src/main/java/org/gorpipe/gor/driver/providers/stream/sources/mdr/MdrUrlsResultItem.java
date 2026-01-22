bc6fc2c49b421581ddf81a3ea5a4f8f911230ee7
Gisli Magnusson <gmagnusson@genedx.com>
2026-01-22 16:46:33 +0000

    fix(ENGKNOW-3080):  MDR changes break GOR. (#97)

--- model/src/main/java/org/gorpipe/gor/driver/providers/stream/sources/mdr/MdrUrlsResultItem.java
+++ model/src/main/java/org/gorpipe/gor/driver/providers/stream/sources/mdr/MdrUrlsResultItem.java
@@ -1,5 +1,8 @@
 package org.gorpipe.gor.driver.providers.stream.sources.mdr;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record MdrUrlsResultItem(String document_id, String file_name, String url, List<MdrUrlsResultItem> grouped) { }
