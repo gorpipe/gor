2d1f26ef191ad040757b37936bfcccebcb2da531
Gisli Magnusson <gmagnusson@genedx.com>
2026-05-20 16:10:43 +0000

    feat(ENGKNOW-3226):  Add min age and min count to versioned link files. (#117)
    * feat(ENGKNOW-3226):  Add min age and min count to versioned link files.
    
    * feat(ENGKNOW-3363):  Add link updateMeta command.
    
    * feat(ENGKNOW-3226):  Add min age and min count to versioned link files.

--- base/src/main/java/org/gorpipe/base/config/PropsHelper.java
+++ base/src/main/java/org/gorpipe/base/config/PropsHelper.java
@@ -1,22 +1,49 @@
 package org.gorpipe.base.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Helper class for system properties related operations.
  *
  */
 public class PropsHelper {
+
+    private static final Logger log = LoggerFactory.getLogger(PropsHelper.class);
+
     /**
-     * Parse boolan system property.
+     * Parse boolean system property.
      * @param name  property name
-     * @param defValue default value
+     * @param defaultValue default value
      * @return boolean value of the property or default value if property is not set or has invalid value.
      */
-    public static boolean getBoolean(String name, boolean defValue) {
-        boolean result = defValue;
-        try {
-            result = Boolean.parseBoolean(System.getProperty(name));
-        } catch (IllegalArgumentException | NullPointerException e) {
+    public static boolean getBoolean(String name, boolean defaultValue) {
+        String value = System.getProperty(name);
+        if (value != null) {
+            try {
+                return Boolean.parseBoolean(value);
+            } catch (IllegalArgumentException | NullPointerException e) {
+                log.warn("Invalid boolean for system property {}='{}'. Using default {}.", name, value, defaultValue);
+            }
         }
-        return result;
+        return defaultValue;
+    }
+
+    /**
+     * Parse int system property.
+     * @param name  property name
+     * @param defaultValue default value
+     * @return int value of the property or default value if property is not set or has invalid value.
+     */
+    public static int getInt(String name, int defaultValue) {
+        String value = System.getProperty(name);
+        if (value != null) {
+            try {
+                return Integer.parseInt(value);
+            } catch (NumberFormatException e) {
+                log.warn("Invalid integer for system property {}='{}'. Using default {}.", name, value, defaultValue);
+            }
+        }
+        return defaultValue;
     }
 }
